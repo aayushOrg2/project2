1016996f8f17bdda0db2261a4a1a375bb3c66f2c
aayushsinghal1984 <aayush.singhal01@gmail.com>
2026-06-30 00:37:22 +0530

    Update outputs.tf

--- modules/resource-group/outputs.tf
+++ modules/resource-group/outputs.tf
@@ -1,11 +1,14 @@
-output "id" {
-  value = azurerm_resource_group.this.id
+output "resource_group_name" {
+  description = "The name of the resource group"
+  value       = azurerm_resource_group.example.name
 }
 
-output "name" {
-  value = azurerm_resource_group.this.name
+output "resource_group_location" {
+  description = "The location of the resource group"
+  value       = azurerm_resource_group.example.location
 }
 
-output "location" {
-  value = azurerm_resource_group.this.location
+output "resource_group_tags" {
+  description = "The tags of the resource group"
+  value       = azurerm_resource_group.example.tags
 }
